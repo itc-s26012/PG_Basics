0a71b09eb74634ea1add4a046edd41cda1a277d6
itc-s26012 <s26012@std.it-college.ac.jp>
2026-06-25 06:36:38 +0000

    P16-P46 第三章演習　終了

---
+++ chapter03/ex3-18.py
@@ -0,0 +1,6 @@
+print(not 1 == 1)
+print(not 1 == 2)
+
+print(not 1 != 1)
+
+print(not 1 != 2)
---
+++ chapter03/ex3-26.py
@@ -0,0 +1,7 @@
+x == 100
+if x == 10:
+    print("10!")
+elif x == 20:
+    print("20!")
+else:
+    print("わかりません！")
